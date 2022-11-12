8670fa2d225b8e31feed905a884916126ef7daab
Jakob Staudal <jakobstaudal@outlook.com>
2022-11-12 14:59:44 +0100

    nothing

--- Cupcake/src/main/java/com/example/cupcake/database/Connection.java
+++ Cupcake/src/main/java/com/example/cupcake/database/Connection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Connection {
     private final String USERNAME = "root";
-    private final String PASSWORD = "root";
+    private final String PASSWORD = "rootroot";
     private final String URL = "jdbc:mysql://localhost:3306/cupcakes";
 
     private java.sql.Connection connect;
